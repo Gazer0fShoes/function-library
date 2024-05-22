2b666cd95d50dd4889fab2ef7f764244e870bd5d
gazer0fShoes <oisin.weitzman@gmail.com>
2024-05-22 14:44:56 +0900

    add factorial

---
+++ math.js
@@ -0,0 +1,11 @@
+// FACTORIAL
+function factorial(num) {
+    if (num < 0) return undefined;
+    if (num === 0) return 1;
+    let x = num - 1;
+    while (x < 1) {
+      num *= x;
+      x--;
+    }
+    return num;
+}
