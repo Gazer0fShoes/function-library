e26eb4904a596725c5939a896d96b63a5ca8cef8
gazer0fShoes <oisin.weitzman@gmail.com>
2024-06-10 15:54:09 +0900

    create array.js, add fisherYatesShuffle

---
+++ array.js
@@ -0,0 +1,12 @@
+/*
+CONTENTS (ctrl + f)
+
+Fisher-Yates Shuffle
+*/
+
+function fisherYatesShuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor( Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
